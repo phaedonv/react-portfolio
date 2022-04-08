637f0993fa04364dffaf80be5bddb5e683cecc70
phaedonv <faival@protonmail.com>
2022-04-08 18:02:11 +0200

    added some project-cards to project-list

--- src/components/projectList/ProjectList.jsx
+++ src/components/projectList/ProjectList.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-import './ProjectList.css'
+import Project from '../project/Project'
+import './projectList.css'
 
 const ProjectList = () => {
   return (
     <div className='pl'>
+        <div className="pl-texts">
+            <h1 className="pl-title">Create & inspire, never stop exploring!</h1>
+            <p className="pl-desc">
+            Here come some of my projects, that you can check and get ispired as I also got.
+            Beautiful colors, useful tools & a whole lot more awaits you!
+            </p>
+        </div>
 
+        <div className='pl-list'>
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+        </div>
     </div>
   )
 }
