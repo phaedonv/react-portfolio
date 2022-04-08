7275cfaae2407e015d8f33c8a2fcc901e50f4b2b
phaedonv <faival@protonmail.com>
2022-04-08 11:42:20 +0200

    building intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./intro.css"
 
 const Intro = () => {
   return (
-    <div>
-        this goes the intro!
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className='i-intro'>Hello, My name is</h2>
+          <h1 className='i-name'>Phaedon V.</h1>
+          <div className="i-title"></div>
+        </div>
+      </div>
+      <div className="i-right">
+        
+      </div>
     </div>
   )
 }
