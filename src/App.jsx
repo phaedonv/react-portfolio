79e7f3b067ff67c01ee1c05e2aadf17cc5eda910
phaedonv <faival@protonmail.com>
2022-04-20 21:05:39 +0200

    installed fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProjectList from "./components/projectList/ProjectList";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
+
+
 const App = () => {
   return (
     <div>
@@ -10,6 +15,10 @@ const App = () => {
       <About/>
       <ProjectList/>
       <Contact/>
+
+      <FontAwesomeIcon icon={solid('user-secret')} />
+      <FontAwesomeIcon icon={regular('coffee')} />
+      <FontAwesomeIcon icon={brands('twitter')} />
     </div>
   )
 }
