753e43f590c334fe80c666d568ecea388940d934
Phaedon Valasiadis <faival@protonmail.com>
2022-09-25 19:25:19 +0200

    swipper-update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -98,6 +98,33 @@ const About = () => {
                         Object-oriented programming, Ruby on Rails, JavaScript, React.js, 
                         HTML5, CSS, SCSS, SASS, SQL, PostgreSQL, Git, Github, Product design, UX/UI, Wireframing, Mockups.
                         </p>
+                        <div id = "skill-swipper">
+                            <Swiper
+                                spaceBetween={30}
+                                centeredSlides={true}
+                                autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                                }}
+                                pagination={{
+                                clickable: true,
+                                }}
+                                navigation={true}
+                                modules={[Autoplay, Pagination, Navigation]}
+                                className="mySwiper"
+                            >
+                                <SwiperSlide>Slide 1</SwiperSlide>
+                                <SwiperSlide>Slide 2</SwiperSlide>
+                                <SwiperSlide>Slide 3</SwiperSlide>
+                                <SwiperSlide>Slide 4</SwiperSlide>
+                                <SwiperSlide>Slide 5</SwiperSlide>
+                                <SwiperSlide>Slide 6</SwiperSlide>
+                                <SwiperSlide>Slide 7</SwiperSlide>
+                                <SwiperSlide>Slide 8</SwiperSlide>
+                                <SwiperSlide>Slide 9</SwiperSlide>
+                            </Swiper>
+    
+                    </div>
                     </div>
                 </div>
             </div>
